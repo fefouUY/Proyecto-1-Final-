c0b97468520b19b640c92290b639e117e38f9e91
Federico Varela <fedeelvareliano@gmail.com>
2022-01-13 20:56:56 -0300

    update 1 react process

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {Analisis,Footer,Navigation,Historia,} from "./components";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <Navigation />
+    <Routes>
+      <Route path="/Historia" element={<Historia />} />
+      <Route path="/Analisis" element={<Analisis/>} />
+    </Routes>
+    <Footer />
+  </Router>,
+
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
