2d6d4884b94aeba332245d00d98daf61f3e16324
fefouUY <fedeelvareliano@gmail.com>
2025-01-08 21:42:08 -0300

    git push

--- src/components/Contactos.jsx
+++ src/components/Contactos.jsx
@@ -1,34 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Contactos (){
-    return(
-        <div className="Contactos">
-                <h1>Contacto</h1>
-                <form onsubmit="return false"></form>
-                <input type="text" placeholder="Ingresa tu nombre" id="Nombre" />
-                <input type="text" placeholder="Ingresa tu apellido" id="Apellido" />
-                <br></br>
-                <input type="radio" id="male" name="gender" value="hombre"> Hombre </input>
-                <input type="radio" id="female" name="gender" value="mujer"> Mujer </input>
-                <br></br>
-                <input type="email" placeholder="Ingresa tu email" id="email" />
-                <select id="country">
-                <option disabled selected value> -- select a country -- </option>
-                <option value="Uruguay">Uruguay</option>
-                <option value="Brasil">Brasil</option>
-                <option value="Otros">Otros</option>
-                </select>
-                <br></br>
-                <input type="password" placeholder="Ingrese Contraseña" id="contraseña" />
-                <input type="password" placeholder="ingrese Contraseña (check)" id="contraseñaChequeada" />
-                <br></br>
-                <input type="checkbox" id="newsletter" />Enviarme noticias mensualmente
-                <br></br>
-                <span>Cual es tu consulta?</span>
-                <br/>
-                <textarea id="question" rows="5"></textarea>
-                <button onclick="sendContact();">Enviar</button>
-        </div>
+function Contactos() {
+  const [country, setCountry] = useState("");
+  const [gender, setGender] = useState("");
+  const sendContact = () => {
+    console.log("Formulario enviado");
+  };
+
+  return (
+    <div className="Contactos">
+      <h1>Contacto</h1>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor="Nombre">Nombre:</label>
+        <input type="text" placeholder="Ingresa tu nombre" id="Nombre" />
+        <br />
+
+        <label htmlFor="Apellido">Apellido:</label>
+        <input type="text" placeholder="Ingresa tu apellido" id="Apellido" />
+        <br />
+
+        <span>Género:</span>
+        <br />
+        <input
+          type="radio"
+          id="male"
+          name="gender"
+          value="hombre"
+          onChange={() => setGender("hombre")}
+        />
+        <label htmlFor="male">Hombre</label>
+        <input
+          type="radio"
+          id="female"
+          name="gender"
+          value="mujer"
+          onChange={() => setGender("mujer")}
+        />
+        <label htmlFor="female">Mujer</label>
+        <br />
+
+        <label htmlFor="email">Email:</label>
+        <input type="email" placeholder="Ingresa tu email" id="email" />
+        <br />
+
+        <label htmlFor="country">País:</label>
+        <select
+          id="country"
+          value={country}
+          onChange={(e) => setCountry(e.target.value)}
+        >
+          <option disabled value="">
+            -- select a country --
+          </option>
+          <option value="Uruguay">Uruguay</option>
+          <option value="Brasil">Brasil</option>
+          <option value="Otros">Otros</option>
+        </select>
+        <br />
+
+        <label htmlFor="contraseña">Contraseña:</label>
+        <input type="password" placeholder="Ingrese Contraseña" id="contraseña" />
+        <br />
+        <label htmlFor="contraseñaChequeada">Confirme Contraseña:</label>
+        <input
+          type="password"
+          placeholder="ingrese Contraseña (check)"
+          id="contraseñaChequeada"
+        />
+        <br />
+
+        <input type="checkbox" id="newsletter" />
+        <label htmlFor="newsletter">Enviarme noticias mensualmente</label>
+        <br />
+
+        <label htmlFor="question">Cual es tu consulta?</label>
+        <br />
+        <textarea id="question" rows="5"></textarea>
+        <br />
+
+        <button onClick={sendContact}>Enviar</button>
+      </form>
+    </div>
   );
 }
 
